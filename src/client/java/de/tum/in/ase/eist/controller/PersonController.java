92771e044987def20ed668ca050b123bc44c46ae
handoko <felix.handoko@tum.de>
2023-05-11 17:33:05 +0200

    a

--- src/client/java/de/tum/in/ase/eist/controller/PersonController.java
+++ src/client/java/de/tum/in/ase/eist/controller/PersonController.java
@@ -3,6 +3,7 @@ package de.tum.in.ase.eist.controller;
 import de.tum.in.ase.eist.model.Note;
 import de.tum.in.ase.eist.model.Person;
 import de.tum.in.ase.eist.util.PersonSortingOptions;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -52,10 +53,30 @@ public class PersonController {
     }
 
     public void deletePerson(Person person, Consumer<List<Person>> personsConsumer) {
-        // TODO Part 2: Make an http delete request to the server
+        webClient.delete()
+                .uri("persons")
+                .retrieve()
+                .toBodilessEntity()
+                .onErrorStop()
+                .subscribe(n -> {
+                    persons.remove(person);
+                    personsConsumer.accept(persons);
+                });
     }
 
     public void getAllPersons(PersonSortingOptions sortingOptions, Consumer<List<Person>> personsConsumer) {
-        // TODO Part 2: Make an https get request to the server
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("persons")
+                        .queryParam("secret", "SecretKey")
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Person>>() {})
+                .onErrorStop()
+                .subscribe(newPersons -> {
+                    persons.clear();
+                    persons.addAll(newPersons);
+                    personsConsumer.accept(persons);
+                });
     }
 }
