46a9cb933b326a2478c2718db38fff26cc3767ec
handoko <felix.handoko@tum.de>
2023-05-11 17:21:13 +0200

    a

--- src/client/java/de/tum/in/ase/eist/H05E01ClientApplication.java
+++ src/client/java/de/tum/in/ase/eist/H05E01ClientApplication.java
@@ -2,11 +2,15 @@ package de.tum.in.ase.eist;
 
 import de.tum.in.ase.eist.controller.NoteController;
 import de.tum.in.ase.eist.controller.PersonController;
+import de.tum.in.ase.eist.model.Person;
 import de.tum.in.ase.eist.view.HomeScene;
 import de.tum.in.ase.eist.view.NoteScene;
 import de.tum.in.ase.eist.view.PersonScene;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 public class H05E01ClientApplication extends Application {
     private final NoteController noteController = new NoteController();
--- src/client/java/de/tum/in/ase/eist/controller/PersonController.java
+++ src/client/java/de/tum/in/ase/eist/controller/PersonController.java
@@ -2,13 +2,39 @@ package de.tum.in.ase.eist.controller;
 
 import de.tum.in.ase.eist.model.Person;
 import de.tum.in.ase.eist.util.PersonSortingOptions;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
 public class PersonController {
 
+    private final WebClient webClient;
+    private final List<Person> persons;
+
+    public PersonController() {
+        this.webClient=WebClient.builder()
+                .baseUrl("http://localhost:8080/")
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+        this.persons = new ArrayList<>();
+    }
+
     public void addPerson(Person person, Consumer<List<Person>> personsConsumer) {
-        // TODO Part 2: Make an http post request to the server
+        webClient.post()
+                .uri("persons")
+                .bodyValue(person)
+                .retrieve()
+                .bodyToMono(Person.class)
+                .onErrorStop()
+                .subscribe(newPerson -> {
+                    persons.add(newPerson);
+                    personsConsumer.accept(persons);
+                });
     }
 
     public void updatePerson(Person person, Consumer<List<Person>> personsConsumer) {
